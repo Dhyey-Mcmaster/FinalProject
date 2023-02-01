79200512c3ea3ca003e79dc62c3dbf21d1164a58
Robern4 <90425812+Robern4@users.noreply.github.com>
2023-01-31 20:23:36 -0500

    Update YF.py

--- YF.py
+++ YF.py
@@ -1,13 +1,92 @@
+# TSLA, SPY, APPLE, META, GOOGLE
 
 import yfinance as yf
 
+TSLA = yf.Ticker('TSLA')
 
-tsla = yf.Ticker('tsla')
+TSLAInfo = TSLA.info
 
-tslaInfo = tsla.info
-
-for key,value in tslaInfo.items():
+for key,value in TSLAInfo.items():
     print(key, ':', value)
     
+tickers = ['tsla']
+for ticker in tickers:
+    ticker_yahoo = yf.Ticker(ticker)
+    data = ticker_yahoo.history()
+    last_quote = data['Close'].iloc[-1]
+    print("Telsa", last_quote)
+    print(" ")
+    print("Open and close")
+    print(data)
+    
+
+SPY = yf.Ticker('SPY')
+
+SPYInfo = SPY.info
+
+for key,value in SPYInfo.items():
+    print(key, ':', value)
+    
+tickers = ['spy']
+for ticker in tickers:
+    ticker_yahoo = yf.Ticker(ticker)
+    data = ticker_yahoo.history()
+    last_quote = data['Close'].iloc[-1]
+    print("SPY", last_quote)
+    print(" ")
+    print("Open and close")
+    print(data)
     
     
+    
+Meta = yf.Ticker('Meta')
+
+MetaInfo = Meta.info
+
+for key,value in MetaInfo.items():
+    print(key, ':', value)
+    
+tickers = ['Meta']
+for ticker in tickers:
+    ticker_yahoo = yf.Ticker(ticker)
+    data = ticker_yahoo.history()
+    last_quote = data['Close'].iloc[-1]
+    print("Meta", last_quote)
+    print(" ")
+    print("Open and close")
+    print(data)
+    
+
+AAPL = yf.Ticker('AAPL')
+
+AAPLInfo = AAPL.info
+
+for key,value in AAPLInfo.items():
+    print(key, ':', value)
+    
+tickers = ['AAPL']
+for ticker in tickers:
+    ticker_yahoo = yf.Ticker(ticker)
+    data = ticker_yahoo.history()
+    last_quote = data['Close'].iloc[-1]
+    print("Apple", last_quote)
+    print(" ")
+    print("Open and close")
+    print(data)
+    
+GOOGL = yf.Ticker('GOOGL')
+
+GOOGLInfo = GOOGL.info
+
+for key,value in SPYInfo.items():
+    print(key, ':', value)
+    
+tickers = ['GOOGL']
+for ticker in tickers:
+    ticker_yahoo = yf.Ticker(ticker)
+    data = ticker_yahoo.history()
+    last_quote = data['Close'].iloc[-1]
+    print("GOOGLE", last_quote)
+    print(" ")
+    print("Open and close")
+    print(data)
